5bb46adb95a1e449f6d8b5b766464d703b185fe1
Max Jaderberg <maxjaderberg@gmail.com>
2011-10-23 00:45:45 +0100

    styling webapp

--- IntelligentImage/settings.py
+++ IntelligentImage/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/jaderberg/Sites/4YP/IntelligentImage/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/jaderberg/Sites/4YP/IntelligentImage/static/',
 )
 
 # List of finder classes that know how to find static files in
