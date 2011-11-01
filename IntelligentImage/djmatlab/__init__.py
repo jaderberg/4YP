5f8de41b8746e927fce5ae3f4b4e748d9fbe810a
Max Jaderberg <maxjaderberg@gmail.com>
2011-11-01 15:50:32 +0000

    matlab function processor is running!!!

--- IntelligentImage/djmatlab/__init__.py
+++ IntelligentImage/djmatlab/__init__.py
@@ -10,6 +10,19 @@ try:
     page = urllib2.urlopen('%s/test_connect.m' % ('http://localhost:4000' if not hasattr(settings, 'MATLAB_SERVER') else settings.MATLAB_SERVER), test_data, 1)
     response = simplejson.loads(page.read())
     print response['message']
+
+    test_data = urllib.urlencode({
+        'func_path': '/Users/jaderberg/Sites/4YP/IntelligentImage/djmatlab/test_sum.m',
+        'arguments': simplejson.dumps({
+            'echo': 'Function processor is working',
+        })
+    })
+    page = urllib2.urlopen('%s/web_feval.m' % ('http://localhost:4000' if not hasattr(settings, 'MATLAB_SERVER') else settings.MATLAB_SERVER), test_data, 1)
+    response = simplejson.loads(page.read())
+    if response['success'] == 'true':
+        print response['result']
+    else:
+        print response['message']
 except urllib2.URLError:
     print 'Matlab server not running'
 
