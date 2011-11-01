4f6574d683dadc30ebc3ce33eb45ebe2d15435ce
Max Jaderberg <maxjaderberg@gmail.com>
2011-11-01 18:29:20 +0000

    cleanup

--- IntelligentImage/djmatlab/__init__.py
+++ IntelligentImage/djmatlab/__init__.py
@@ -1,18 +1,10 @@
-import urllib2,urllib
 from django.conf import settings
-from django.utils import simplejson
-import os
-
+from djmatlab.pymatlab import Matlab
 
 # Check Matlab webserver is running
-from djmatlab.utils import Matlab
-
-MATLAB_FOLDER = '%s/matlab' % os.path.realpath(os.path.dirname(__file__))
 
 matlab = Matlab('http://localhost:4000' if not hasattr(settings, 'MATLAB_SERVER') else settings.MATLAB_SERVER, port=None if not hasattr(settings, 'MATLAB_SERVER_PORT') else settings.MATLAB_SERVER_PORT)
-try:
-    print 'djmatlab - Connected: ', matlab.is_connected()
-    print 'djmatlab - Function processor working: ', matlab.is_function_processor_working()
-except urllib2.URLError:
-    print 'djmatlab - Matlab server not running'
+print 'djmatlab - Connected: ', matlab.is_connected()
+print 'djmatlab - Function processor working: ', matlab.is_function_processor_working()
+
 
