e81d7cbcc20095919662a8369a9fd44c25c92cb0
Max Jaderberg <maxjaderberg@gmail.com>
2011-11-09 14:48:23 +0000

    multi matching done

--- visualindex/demo_getobjects.m
+++ visualindex/demo_getobjects.m
@@ -8,6 +8,8 @@ function result = demo_getobjects(args)
         display = args.display;
     end
     
+    figures = 1;
+    
     % setup VLFeat
     run /Users/jaderberg/Sites/4YP/visualindex/vlfeat/toolbox/vl_setup ;
 
@@ -39,62 +41,95 @@ function result = demo_getobjects(args)
 
     if display
     %     Plot image
-        figure(1) ; clf ;
+        figure(figures) ; clf ; figures = figures + 1;
         imagesc(im) ; title(sprintf('Query: %s', imagePath)) ;
         axis image off ; drawnow ;
     end
     
-%     Get matches
-%     [ids, scores, matches] = visualindex_query(model, im) ;
-    [ids, scores, matches] = visualindex_query(model, im, 'exclude', [48.9584 224.6665 245.0729 276.9313]) ;
-%     [ids, scores, matches] = visualindex_query(model, im, 'exclude', [48.9584 224.6665 245.0729 276.9313; 461.2021 212.9303 317.7049 146.0708]) ;
-
+    
+    max_objects = 3;
+    max_tries_per_object = 2;
+    
     result.query_image.path = imagePath;
     result.query_image.sz = sz;
     result.matches = {};
+    result.classes = [];
     
-    if scores(1) < 5
-%         There's no recognisable object in this image :(
-        fprintf('Query Complete - no objects found');
-        return
-    end
+    exclusion_matrix = [];
+    pass_number = 1;
+    current_tries = 0;
     
-    match_image.id = find(imdb.images.id == ids(1));
-    match_image.class = imdb.images.class(match_image.id)
-    match_image.sz = imdb.images.size(:, match_image.id);
-    match_image.path = fullfile(imdb.dir, imdb.images.name{match_image.id});
-    match_image.image = imread(match_image.path);
-    match_image.matches = matches{1};
-    
-    if display
-    %     Plot matched image
-        figure(2); clf;
-        imagesc(match_image.image) ; title(sprintf('Best match: %s', match_image.path)) ;
-        axis image off ; drawnow ;
-    end
+    while pass_number < max_objects*max_tries_per_object
+       
+        [ids, scores, matches] = visualindex_query(model, im, 'exclude', exclusion_matrix) ;
 
-%     Plot matches on query image and bounding rectangle
-    match_coords_x = match_image.matches.f2(1,:);
-    match_coords_y = match_image.matches.f2(2,:);
-    xmin = min(match_coords_x); ymin = min(match_coords_y); xmax = max(match_coords_x); ymax = max(match_coords_y);
-    width = xmax - xmin; height = ymax - ymin;    % rect width and height
-    rect = [xmin ymin width height]
-    if display
-        figure(1);
-        vl_plotframe([match_image.matches.f2]);
-        rectangle('Position', rect, 'EdgeColor', 'r');
-        figure(3) ; clf ;
-        visualindex_plot_matches(model, match_image.matches, match_image.image, im, match_image.sz, sz) ;
+        if scores(1) < 6
+%             There's no recognisable object in this image :(
+            fprintf('Query Complete - no objects found');
+            return
+        end
+        
+        match_image.id = find(imdb.images.id == ids(1));
+        match_image.class = imdb.images.class(match_image.id);
+        
+        if find(result.classes == match_image.class)
+%             This object has already been found in the image
+            current_tries = current_tries + 1;
+            if current_tries >= max_tries_per_object
+                break
+            else
+                continue
+            end
+        else
+            result.classes(end+1) = match_image.class;
+        end
+        
+        match_image.sz = imdb.images.size(:, match_image.id);
+        match_image.path = fullfile(imdb.dir, imdb.images.name{match_image.id});
+        match_image.image = imread(match_image.path);
+        match_image.matches = matches{1};
+        
+        if display
+%             Plot matched image
+            figure(figures); clf; figures = figures + 1;
+            imagesc(match_image.image) ; title(sprintf('Best match: %s', match_image.path)) ;
+            axis image off ; drawnow ;
+        end
+
+%         Plot matches on query image and bounding rectangle
+        match_coords_x = match_image.matches.f2(1,:);
+        match_coords_y = match_image.matches.f2(2,:);
+        xmin = min(match_coords_x); ymin = min(match_coords_y); xmax = max(match_coords_x); ymax = max(match_coords_y);
+        width = xmax - xmin; height = ymax - ymin;    % rect width and height
+        rect = [xmin ymin width height];
+        if display
+            figure(1);
+            vl_plotframe([match_image.matches.f2]);
+            rectangle('Position', rect, 'EdgeColor', 'r');
+            figure(figures) ; clf ; figures = figures + 1;
+            visualindex_plot_matches(model, match_image.matches, match_image.image, im, match_image.sz, sz) ;
+        end
+
+
+        match.rectangle.top = ymax; match.rectangle.left = xmin;
+        match.rectangle.width = width; match.rectangle.height = height;
+        match.path = match_image.path;
+
+%         add match to results
+        result.matches{1} = match;
+        
+%         add rectangle of match to exclusion region
+        exclusion_matrix(end+1,:) = rect;
+        
+        if length(result.matches) >= max_objects
+            break            
+        end
+        
+        pass_number = pass_number + 1;
     end
     
     
-    match.rectangle.top = ymax; match.rectangle.left = xmin;
-    match.rectangle.width = width; match.rectangle.height = height;
-    match.path = match_image.path;
-    
-    result.matches{1} = match;
-    
-    fprintf('Query Complete - objects found');
+    fprintf('Query Complete - object(s) found');
     
     
     
