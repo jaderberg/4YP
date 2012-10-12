a99dc0d3702a324a3379a1e032e0500991cdf59a
Max Jaderberg <maxjaderberg@gmail.com>
2012-10-12 12:39:24 +0100

    renamed vocab.mat to album_vocab.mat

--- Precomputation/fabfile.py
+++ Precomputation/fabfile.py
@@ -85,7 +85,7 @@ def full_precompute():
 
     skip_vocab = confirm('Use existing vocab?', default=True)
     if skip_vocab:
-        vocab_file = prompt('Existing vocab.mat file to use: ', default='/Users/jaderberg/Sites/4YP/Precomputation/kebl3465@engs-station49.eng.ox.ac.uk/vocab.mat')
+        vocab_file = prompt('Existing vocab.mat file to use: ', default='/Users/jaderberg/Sites/4YP/Precomputation/kebl3465@engs-station49.eng.ox.ac.uk/album_vocab.mat')
         root_dir = prompt('Project root dir: ', default='~/4YP/data/%s' % data_dir)
 
     tasks = []
